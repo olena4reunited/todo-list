fa0315efce4e00302acbe8688e92649c18672701
olena4reunited <olenaduringintermission@gmail.com>
2024-09-25 12:43:29 +0300

    build: create tag_list view and template; update templates and urls

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from todo_app.views import HomeView, TaskCreateView, TaskUpdateView, TaskDeleteView
+from todo_app.views import HomeView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagListView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("create/", TaskCreateView.as_view(), name="create-task"),
     path("<int:pk>/update/", TaskUpdateView.as_view(), name="update-task"),
     path("<int:pk>/delete/", TaskDeleteView.as_view(), name="delete-task"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo-app"
